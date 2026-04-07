f5d561c683b18d6f7979fc079ff2a7765fa52f48
4ernavskaya <vasilisaie27@gmail.com>
2026-04-07 12:11:07 +0500

    Add files via upload
    добавила проект

---
+++ Robots-main/ucheba/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Oko");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+    }
+
+    public class Top{
+
+
+    }
+}
